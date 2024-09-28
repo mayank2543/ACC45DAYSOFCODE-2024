25657323ecd0387edb6b9c8f21f20a606916225f
mayank2543 <mayankrajpoot60@gmail.com>
2024-09-28 14:30:58 +0530

    Add files via upload

---
+++ DAY-1 CWC23QUALIF.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+
+     // Here x is point in cricket to qualify
+    scanf("%d", & x);
+
+    if (x >= 12) {
+        printf("Yes \n");
+    }
+    else {
+        printf("No \n");
+    }
+
+
+    return 0;
+
+}
