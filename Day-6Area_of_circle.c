aea267db9391709cf4defc51d7a521c1241d2e39
mayank2543 <mayankrajpoot60@gmail.com>
2024-10-03 22:51:54 +0530

    Add files via upload

---
+++ Day-6Area_of_circle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    float r;
+    printf("enter r :");
+    scanf("%f", &r);
+    float pi = 3.14;
+    
+
+    printf("area is : %f", pi*r*r);
+
+    return 0;
+}
+
